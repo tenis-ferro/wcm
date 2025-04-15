8f9652da9450f28b6dff6a916a3b65402ca4bac2
Fernando Ibarra <fibarra@adnet.cl>
2025-04-15 10:43:19 -0400

    Actualizacion pagina resultados

--- torneos/2025/escalerilla/js/main.js
+++ torneos/2025/escalerilla/js/main.js
@@ -20,7 +20,8 @@ async function cargarResultados() {
         loadMsg:'Leyendo resultado de los partidos',
         title:'Resultado de partidos',
         iconCls:'icon-ok',
-        fitColumns:true,
+        //rownumbers: true,
+        //fitColumns:true,
         columns:[[
             {field:'Nro',title:'#',width:35},
             {field:'Fecha',title:'Fecha', width:100, formatter:function(val, row, idx){               
@@ -30,17 +31,19 @@ async function cargarResultados() {
                 //console.log(formattedDate,idx)
                 return formattedDate;
             }},
-            {field:'Ganador',title:'Ganador',width:150,align:'left',formatter:function(val, row, idx){               
+            {field:'Ganador',title:'Ganador',width:200,align:'left',formatter:function(val, row, idx){               
               return '<span style="color:black;font-weight:bold;">'+val+'</span>';
             }},
-            {field:'Jugador 1',title:'Partido', width:300,align:'left', formatter:function(val, row, idx){               
+            {field:'Jugador 1',title:'Partido', width:350,align:'left', formatter:function(val, row, idx){               
               //console.log(row,idx)
               let partido = '';
               return partido.concat(row["Jugador 1"],' / ', row["Jugador 2"]);
             }},
-            {field:'Resultado',title:'Resultado',width:100,align:'left'},
+            {field:'Resultado',title:'Resultado',width:150,align:'left'},
             {field:'Categoria',title:'Categoria',width:80,align:'left'}
-        ]]
+        ]],
+        sortName: 'Nro',
+        sortOrder: 'Asc'
         });
         
         $('#dg').datagrid('enableFilter');
