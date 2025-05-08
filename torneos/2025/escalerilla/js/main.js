7e96bfa15ef52f2b1c1e27909eb7cdcc8ec44080
Fernando Ibarra <fibarra@adnet.cl>
2025-05-08 11:33:20 -0400

    Termino fase 1 04

--- torneos/2025/escalerilla/js/main.js
+++ torneos/2025/escalerilla/js/main.js
@@ -2,8 +2,12 @@ var arrResult;
 var arrRnk;
 
 async function cargarResultados() {
-    const url = "https://script.google.com/macros/s/AKfycbyP1LxSSrheRTXQT4YvQZGNOIggL7g0PqzLsLc5vCNsmmQJL3HdJLTklrbKe7Wc4i-Y/exec?path=Partidos-Grupo&action=read";
-    const url2 = "https://script.google.com/macros/s/AKfycbyP1LxSSrheRTXQT4YvQZGNOIggL7g0PqzLsLc5vCNsmmQJL3HdJLTklrbKe7Wc4i-Y/exec?path=Rnk-Grupos&action=read";
+    //const url = "https://script.google.com/macros/s/AKfycbyP1LxSSrheRTXQT4YvQZGNOIggL7g0PqzLsLc5vCNsmmQJL3HdJLTklrbKe7Wc4i-Y/exec?path=Partidos-Grupo&action=read";
+    //const url2 = "https://script.google.com/macros/s/AKfycbyP1LxSSrheRTXQT4YvQZGNOIggL7g0PqzLsLc5vCNsmmQJL3HdJLTklrbKe7Wc4i-Y/exec?path=Rnk-Grupos&action=read";
+
+    const url = "/torneos/2025/escalerilla/data/resultados.json";
+    const url2 = "/torneos/2025/escalerilla/data/clasificados.json";    
+
     try {
       const response = await fetch(url);
       if (!response.ok) {
